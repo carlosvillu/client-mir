2f1dc01fa9ff991109a9be8f23147b89587fa710
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2011-08-20 11:17:37 +0200

    Change port to the new app

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(11311);
+app.listen(11468);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
